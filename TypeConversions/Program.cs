8ede3a539603d555f4a1dc2baeff1c39d4ea72f1
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-09 00:24:20 -0600

    code complete.

--- TypeConversions/Program.cs
+++ TypeConversions/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+
+//parse
+//int age = int.Parse(ageText);
+
+//tryparse
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
+
+Console.WriteLine(age + 15);
+
+//casting
+double testDouble = age;
+decimal testDecimal = (decimal)testDouble;
